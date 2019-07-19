a6d6a09ddd36d961a378436e661898bbf33e5f0b
Tùng Nguyễn <tungnguyen@Tungs-MacBook-Pro.local>
2019-07-20 00:30:34 +0700

    + Learn body-parser and CORS

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan'); 
+const bodyParser = require('body-parser');
 
 const productRoute  = require('./api/routes/Products');
 const odersRoute    = require('./api/routes/Oders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// for CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Header', 'Origin, X-Requested-with, Content-Type, Accept, Authorization');
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json()
+    }
+    next();
+});
+
+// for Route
 app.use('/products', productRoute);
 app.use('/oders', odersRoute);
 
--- api/routes/Oders.js
+++ api/routes/Oders.js
@@ -8,8 +8,13 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
+    const oder = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    }
     res.status(201).json({
-        message: 'Oders was craeted'
+        message: 'Oders was craeted',
+        oder: oder
     });
 });
 
